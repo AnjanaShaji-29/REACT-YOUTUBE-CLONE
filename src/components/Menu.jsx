9a1c73f43825c5765ca4ca66e7f9cef533b4f499
AnjanaShaji29 <AnjanaShaji29>
2023-10-06 10:36:43 +0530

    Menu Design added

---
+++ src/components/Menu.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import styled from 'styled-components';
+import Youtubelogo from "../img/logo.png";
+import { AccountCircleOutlined, ArticleOutlined, ExploreOutlined, FlagOutlined, HelpOutlineOutlined, HistoryOutlined, Home, LibraryMusicOutlined, LiveTvOutlined, MovieOutlined, SettingsBrightnessOutlined, SettingsOutlined, SportsEsportsOutlined, SubscriptionsOutlined, VideoLibraryOutlined } from '@mui/icons-material';
+
+
+const Container = styled.div`
+    flex: 1;
+    height: 110vh;
+    color: white;
+    background-color: black;
+    font-size: 14px;
+    position: sticky;
+    top:0;
+`;
+
+const Wrapper = styled.div`
+    padding: 18px 26px;
+`;
+
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    font-weight: bold;
+    margin-bottom: 25px;
+`;
+
+const Img = styled.img`
+    height: 25px;
+`;
+
+const Item = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    cursor: pointer;
+    padding: 6px 0px;
+`;
+
+const Hr = styled.hr`
+    margin: 15px 0px;
+    border: 0.5px solid #373737;
+`;
+
+const Login = styled.div`
+
+`;
+
+const Button = styled.button`
+    padding:5px 15px;
+    background-color:transparent;
+    border: 1px solid #3ea6ff;
+    color: #3ea6ff;
+    border-radius: 3px;
+    font-weight: 500;
+    margin-top: 10px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap:5px;
+`;
+
+export default function Menu() {
+  return (
+    <Container>
+    <Wrapper>
+    <Logo>
+        <Img src={Youtubelogo} /> Youtube
+    </Logo>
+    <Item>
+      <Home/> Home
+    </Item>
+    <Item>
+    <ExploreOutlined /> Explore
+    </Item>
+    <Item>
+        <SubscriptionsOutlined/> Subscriptions
+    </Item>
+    <Hr />
+    <Item>
+        <VideoLibraryOutlined/> Library
+    </Item>
+    <Item>
+        <HistoryOutlined /> History
+    </Item>
+    <Hr />
+    <Login> Sign in to like videos, comment and subscribe.
+        <Button> <AccountCircleOutlined /> SIGN IN </Button>
+    </Login>
+    <Hr />
+    <Item>
+        <LibraryMusicOutlined/> Music
+    </Item>
+    <Item>
+        <SportsEsportsOutlined/> Gaming
+    </Item>
+    <Item>
+        <MovieOutlined/> Movies
+    </Item>
+    <Item>
+        <ArticleOutlined /> News
+    </Item>
+    <Item>
+        <LiveTvOutlined/> Live
+    </Item>
+    <Hr />
+    <Item>
+        <SettingsOutlined /> Settings
+    </Item>
+    <Item>
+        <FlagOutlined /> Report
+    </Item>
+    <Item>
+        <HelpOutlineOutlined /> Help
+    </Item>
+    <Item>
+        <SettingsBrightnessOutlined/> Light Mode
+    </Item>
+    </Wrapper>
+    </Container>
+  )
+}
